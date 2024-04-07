0fbfc4f33449f234a128197387af1990d5876df7
Natalia Traczewska <165217683+NataliaTrac@users.noreply.github.com>
2024-04-07 22:36:42 +0200

    Create puzzle.py

---
+++ Knights/puzzle.py
@@ -0,0 +1,77 @@
+from logic import *
+
+AKnight = Symbol("A is a Knight")
+AKnave = Symbol("A is a Knave")
+
+BKnight = Symbol("B is a Knight")
+BKnave = Symbol("B is a Knave")
+
+CKnight = Symbol("C is a Knight")
+CKnave = Symbol("C is a Knave")
+
+characterDefinitions = And(
+    Or(AKnight, AKnave), Not(And(AKnight, AKnave)),
+    Or(BKnight, BKnave), Not(And(BKnight, BKnave)),
+    Or(CKnight, CKnave), Not(And(CKnight, CKnave))
+)
+
+
+knowledge0 = And(
+    characterDefinitions,
+    Implication(AKnight, And(AKnight, AKnave)),
+    Implication(AKnave, Not(And(AKnight, AKnave)))
+)
+
+# Puzzle 1
+# A says "We are both knaves."
+# B says nothing.
+knowledge1 = And(
+    characterDefinitions,
+    Implication(AKnight, And(AKnave, BKnave)),
+    Implication(AKnave, Not(And(AKnave, BKnave)))
+)
+
+# Puzzle 2
+# A says "We are the same kind."
+# B says "We are of different kinds."
+knowledge2 = And(
+    characterDefinitions,
+    Implication(AKnight, BKnight),
+    Implication(BKnight, AKnave),
+    Implication(AKnave, BKnight),
+    Implication(BKnave, AKnight)
+)
+
+# Puzzle 3
+# A says either "I am a knight." or "I am a knave.", but you don't know which.
+# B says "A said 'I am a knave'."
+# B says "C is a knave."
+# C says "A is a knight."
+knowledge3 = And(
+    characterDefinitions,
+    Biconditional(AKnight, Or(AKnight, AKnave)),
+    Implication(BKnight, And(Not(AKnight), CKnave)),
+    Implication(CKnight, AKnight)
+)
+
+
+def main():
+    symbols = [AKnight, AKnave, BKnight, BKnave, CKnight, CKnave]
+    puzzles = [
+        ("Puzzle 0", knowledge0),
+        ("Puzzle 1", knowledge1),
+        ("Puzzle 2", knowledge2),
+        ("Puzzle 3", knowledge3)
+    ]
+    for puzzle, knowledge in puzzles:
+        print(puzzle)
+        if len(knowledge.conjuncts) == 0:
+            print("    Not yet implemented.")
+        else:
+            for symbol in symbols:
+                if model_check(knowledge, symbol):
+                    print(f"    {symbol}")
+
+
+if __name__ == "__main__":
+    main()
